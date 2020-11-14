3a02f656c99f028e4bc0fca18dd9b39aacf8ca0d
kkaniap <xkaniax@gmail.com>
2020-11-14 10:41:10 +0100

    initial commit

---
+++ src/main/java/com/terrasi/terrasirpi/TerrasirpiApplication.java
@@ -0,0 +1,13 @@
+package com.terrasi.terrasirpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerrasirpiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerrasirpiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/terrasi/terrasirpi/TerrasirpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terrasi.terrasirpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerrasirpiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
